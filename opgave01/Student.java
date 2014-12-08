239c620436b04542e7190815853be29bbf7fc926
Roelof Kallenkoot <roelof000@gmail.com>
2014-12-08 17:56:35 +0100

    Student JavaDoc

--- opgave01/Student.java
+++ opgave01/Student.java
@@ -1,9 +1,10 @@
 
 /**
- * Write a description of class Student here.
+ * class Student here
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Roelof Kallenkoot
+ * @author Iris Meijer
+ * @version 08/12/2014
  */
 public class Student extends Persoon
 {
@@ -13,31 +14,55 @@ public class Student extends Persoon
 
     /**
      * Constructor for objects of class Student
+     * @param Studentnummer
+     * @param Studierichting
+     * @param Burgerservicenummer
+     * @param Voornaam
+     * @param Achternaam
+     * @param Geslacht van de studenet
+     * @param Dag van geboorte van de student
+     * @param Maand van geboorte van de student
+     * @param Geboortejaar van de student
      */
-    public Student(String studentnummer, String studierichting, String bsn, String voornaam, String achternaam, char geslacht,
-    int dag, int maand, int jaar)
+    public Student(String studentnummer, String studierichting, String bsn, String voornaam, 
+    String achternaam, char geslacht, int dag, int maand, int jaar)
     {
-
         super(bsn,voornaam,achternaam,geslacht,dag,maand,jaar);
         this.studentnummer = studentnummer;
         this.studierichting = studierichting;
     }
 
+    /**
+     * Retourneerd het studentnummer
+     * @return Studentnummer
+     */
     public String getStudentnummer()
     {
         return studentnummer;
     }
-
+    
+    /**
+     * Retourneerd de studierichting van de student
+     * @return Studierichting
+     */
     public String getStudierichting()
     {
         return studierichting;
     }
-
+    
+    /**
+     * Setter voor studentnummer
+     * @param Studentnummer
+     */
     public void setStudentnummer(String studentnummer)
     {
         this.studentnummer = studentnummer;
     }
 
+    /**
+     * Setter voor studierichting
+     * @param Studierichting
+     */
     public void setStudierichting(String studierichting)
     {
         this.studierichting = studierichting;
