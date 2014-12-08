d645d0680eaa13d24c5912f535af01ae40fb3066
Roelof Kallenkoot <roelof000@gmail.com>
2014-12-08 17:47:46 +0100

    KantineMedewerker klaar

---
+++ opgave01/KantineMedewerker.java
@@ -0,0 +1,61 @@
+
+/**
+ *  class KantineMedewerker
+ * 
+ * @author Roelof Kallenkoot
+ * @version (a version number or a date)
+ */
+public class KantineMedewerker extends Persoon
+{
+    // Instantievariabelen
+    private String medewerkersnummer;
+    private boolean magAchterKassa;
+
+    /**
+     * Constructor for objects of class KantineMedewerker
+     * @param medewerkersnummer
+     * @param magAchterKassa
+     */
+    public KantineMedewerker(String medewerkersnummer, boolean magAchterKassa)
+    {s
+        // initialise instance variables
+        this.medewerkersnummer = medewerkersnummer;
+        this.magAchterKassa = magAchterKassa;
+    }
+
+    /**
+     * Retourneerd het medewerkersnummer
+     * @return medewerkersnummer
+     */
+    public String getMedewerkersnummer()
+    {
+        return medewerkersnummer;
+    }
+    
+    /**
+     * Retourneerd of de Kantinemedewerker achter de kassa mag of niet
+     * @return magAchterKassa
+     */
+    public boolean getMagAchterKassa()
+    {
+        return magAchterKassa;
+    }
+    
+    /**
+     * Setter voor het medewerkernummer
+     * @param Medewerkersnummer
+     */
+    public void setMedewerkersnummer(String medewerkersnummer)
+    {
+        this.medewerkersnummer = medewerkersnummer;
+    }
+    
+    /**
+     * Setter voor magAchterKassa
+     * @param MagAchterKassa
+     */
+    public void setMagAchterKassa(boolean magAchterKassa)
+    {
+        this.magAchterKassa = magAchterKassa;
+    }
+}
