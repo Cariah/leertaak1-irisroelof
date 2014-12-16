22e50c0304d2f311c577b1e39016371df909d9c2
irismeijer <i.e.meijer@hotmail.com>
2014-12-16 10:49:47 +0100

    Pinpas toegevoegd
    tadaa

--- opgave01/Contant.java
+++ opgave01/Contant.java
@@ -6,12 +6,13 @@
  * @author Iris Meijer
  * @version 16/12/2014
  */
-public class Contant
+public class Contant extends Betaalwijze
 {
     /**
      * Methode om betaling af te handelen
      */
     public boolean betaal(double tebetalen) {
         // omitted
+        return true;
     }
 }
