ffffb02fbcc0f6e1e3b450e94fc46bd39e0da72f
irismeijer <i.e.meijer@hotmail.com>
2014-12-16 13:04:39 +0100

    Betaalmethode in pinpas gefixt
    tadaa

--- opgave01/Pinpas.java
+++ opgave01/Pinpas.java
@@ -7,9 +7,27 @@
  * @version 16-12-2014
  */
 public class Pinpas extends Betaalwijze {
-    
     private double kredietlimiet;
+
+    /**
+     * default constructor voor pinpas
+     */
+    public Pinpas()
+    {
+        setSaldo(0);
+        setKredietLimiet(150);
+    }
     
+    /**
+     * Constructor van pinpas
+     * @param double saldo
+     * @param double kredietlimiet
+     */
+    public Pinpas(double saldo, double kredietlimiet){
+        setSaldo(saldo);
+        setKredietLimiet(kredietlimiet);
+    }
+
     /**
      * Methode om kredietlimiet te zetten
      * @param kredietlimiet
@@ -20,9 +38,22 @@ public class Pinpas extends Betaalwijze {
 
     /**
      * Methode om betaling af te handelen
+     * @param double tebetalen
      */
     public boolean betaal(double tebetalen) {
-        //omitted
-        return true;
+        double som = saldo + kredietlimiet;
+        if(tebetalen <= som){
+            if(saldo >= tebetalen){
+                saldo-= tebetalen;
+            }
+            if((saldo - tebetalen) < 0){
+                tebetalen-= saldo;
+                setSaldo(0);
+                kredietlimiet -= tebetalen;
+            }
+            return true;
+        }
+        
+        return false;
     }
 }
