cd7e43a39f5254c3e14223508abdcbf0e623e758
Roelof Kallenkoot <roelof000@gmail.com>
2014-12-16 10:46:06 +0100

    Betaalwijze en Contant week 5 opgave 2a

---
+++ opgave01/Betaalwijze.java
@@ -0,0 +1,29 @@
+
+/**
+ * Abstract class Betaalwijze - write a description of the class here
+ * 
+ * @author Roelof Kallenkoot
+ * @author Iris Meijer
+ * @version 16/12/2014
+ */
+public abstract class Betaalwijze
+{
+    protected double saldo;
+
+    /**
+     * Methode om krediet te initialiseren	
+     * @param krediet
+     */
+    public void setSaldo(double saldo){
+
+        this.saldo=saldo;
+    }	
+
+    /**
+     * Methode om betaling af te handelen
+     * @param tebetalen
+     * * @return Boolean om te kijken of er voldoende saldo is
+     */
+    public abstract boolean betaal(double tebetalen);
+
+}
---
+++ opgave01/Contant.java
@@ -0,0 +1,17 @@
+
+/**
+ * Contant Class
+ * 
+ * @author Roelof Kallenkoot
+ * @author Iris Meijer
+ * @version 16/12/2014
+ */
+public class Contant
+{
+    /**
+     * Methode om betaling af te handelen
+     */
+    public boolean betaal(double tebetalen) {
+        // omitted
+    }
+}
